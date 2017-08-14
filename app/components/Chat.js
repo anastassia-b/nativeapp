bdcccbc1d4de788ad0cff633382211856a44fc30
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-13 19:01:00 -0700

    Add navigation button

--- app/components/Chat.js
+++ app/components/Chat.js
@@ -3,9 +3,20 @@ import { Text, View, Button } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 class ChatScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => ({
-    title: `Chat with ${navigation.state.params.user}`,
-  });
+  static navigationOptions = ({ navigation }) => {
+    const {state, setParams} = navigation;
+    const isInfo = state.params.mode === 'info';
+    const {user} = state.params;
+
+    return {
+
+      title: isInfo ? `${user}'s Contact Info` : `Chat with ${state.params.user}`,
+      headerRight: (
+        <Button title={isInfo? 'Done' : `${user}'s Info`}
+          onPress={() => setParams({ mode: isInfo ? 'none' : 'info'})}/>
+      )
+    }
+  };
   render() {
     const { params } = this.props.navigation.state;
     return (
