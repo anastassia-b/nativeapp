e31c236d7a9ee47ada6d5fa4439f2b186a739db4
Anastassia Bobokalonova <email.anastassia@gmail.com>
2017-08-13 18:19:40 -0700

    Add Tab Navigation

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { AppRegistry, StyleSheet, View, Text, Button } from 'react-native';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 import ChatScreen from './Chat';
 import HomeScreen from './Home';
 
+class RecentChatsScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>List of recent chats</Text>
+        <Button
+          onPress={() => this.props.navigation.navigate(
+          'Chat', { user: 'David'})} title="Chat with David"
+        />
+      </View>
+    )
+  }
+}
+
+class AllContactsScreen extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>List of all contacts</Text>
+        <Button
+          onPress={() => this.props.navigation.navigate(
+          'Chat', { user: 'Elliot'})} title="Chat with Elliot"
+        />
+      </View>
+    )
+  }
+}
+
+const MainScreenNavigator = TabNavigator({
+  Recent: { screen: RecentChatsScreen },
+  All: { screen: AllContactsScreen }
+});
+
+MainScreenNavigator.navigationOptions = {
+  title: 'My Chats',
+};
+
 const ChatApp2 = StackNavigator({
-  Home: { screen: HomeScreen },
+  Home: { screen: MainScreenNavigator },
   Chat: { screen: ChatScreen },
 });
 
